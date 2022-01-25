43cd16b15fe3b2862e696273727ed0a4a898c04a
dakotasmith13 <97972068+dakotasmith13@users.noreply.github.com>
2022-01-25 18:55:18 -0500

    Created main sitemap

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Dakota's CS008 Website</title>
+        <meta name="author" content="Dakota Smith">
+        <meta name="description" content="A site map for all of my assignments for CS008.">
+    </head>
+
+    <body>
+        <figure style="float: left; text-align: center; width: 20%;">
+            <img alt="Dakota Smith Summer of '21" 
+                 src="images/dakota-smith.jpg" 
+                 style="max-width: 100%;">
+            <figcaption>On top of Mount Pemigewasset.</figcaption>
+        </figure>
+
+        <h1>CS 008-<em>OL</em> Spring 2022</h1>
+        <h2>Dakota Smith - Site map</h2>
+        <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
+
+
+        <h2>Lab one - Setting up your sitemap.</h2>
+        <p><a href="testing.php">Testing Page</a></p>
+
+    </body>
+</html>
