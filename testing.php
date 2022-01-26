4e96131f58d94003f04993253c5d6d8639c09efb
dakotasmith13 <97972068+dakotasmith13@users.noreply.github.com>
2022-01-25 19:01:29 -0500

    Created a test page for myself

---
+++ testing.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Demo page for me</title>
+        <meta name="author" content="Dakota Smith">
+        <meta name="description" content="not for public">
+    </head>
+
+    <body>
+        <h1>Dakota Smith Testing Page</h1>
+        <p> This will be where I test my content</p>
+    </body>
+</html>
