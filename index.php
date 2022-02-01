d51528bdeb6cd60eddc74b429c5a8d93a15d5792
dakotasmith13 <97972068+dakotasmith13@users.noreply.github.com>
2022-02-01 16:16:09 -0500

    Added lab 2 to sitemap

--- index.php
+++ index.php
@@ -19,6 +19,23 @@
         <h2>Dakota Smith - Site map</h2>
         <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
 
+        <section class="grid-layout">
+            <h2 class="header">Lab Two - Making the world a better place</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="lab2/index.php">index.php</a></p>
+            </section>
+
+            <section class="supporting-files">
+                <h3>Supporting files</h3>
+                <p><a href="lab2/css/custom.css">custom.css</a> (if you do the extra credit)</p>
+            </section>
+
+            <section class="grader-notes">
+                <h3>Notes to grader</h3>
+                <p></p>
+            </section> 
+        </section>
 
         <h2>Lab one - Setting up your sitemap.</h2>
         <p><a href="testing.php">Testing Page</a></p>
