0e418a37112292065af18dc56c47ca5f9d89cb14
dakotasmith13 <97972068+dakotasmith13@users.noreply.github.com>
2022-02-06 22:05:41 -0500

    Added lab 3 sections

--- index.php
+++ index.php
@@ -18,6 +18,24 @@
         <h1>CS 008-<em>OL</em> Spring 2022</h1>
         <h2>Dakota Smith - Site map</h2>
         <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
+        
+        <section class="grid-layout">
+            <h2 class="header">Lab Three - Adding detail and a form.</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="lab3/index.php">index.php</a></p>
+            </section>
+
+            <section class="supporting-files">
+                <h3>Supporting files</h3>
+                <p><a href="lab3/css/custom.css">custom.css</a></p>
+            </section>
+
+            <section class="grader-notes">
+                <h3>Notes to grader</h3>
+                <p></p>
+            </section> 
+        </section>
 
         <section class="grid-layout">
             <h2 class="header">Lab Two - Making the world a better place</h2>
@@ -37,8 +55,16 @@
             </section> 
         </section>
 
-        <h2>Lab one - Setting up your sitemap.</h2>
-        <p><a href="testing.php">Testing Page</a></p>
+        <section class="grid-layout">
+            <h2 class="header">Lab one - Setting up your sitemap.</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="testing.php">Testing Page</a></p>
+            <section class="grader-notes">
+                <h3>Notes to grader</h3>
+                <p></p>
+            </section>
+        </section>
 
     </body>
 </html>
