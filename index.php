f0cd37a7771df334433bed8ec402b245fbd96e54
dakotasmith13 <97972068+dakotasmith13@users.noreply.github.com>
2022-03-28 21:21:23 -0400

    Lab 7: updated site map

--- index.php
+++ index.php
@@ -89,6 +89,32 @@
         <h2>Dakota Smith - Site map</h2>
         <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
         
+
+        <section class="grid-layout">
+            <h2 class="header">Lab Seven - Arrays and Includes.</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="lab7/index.php">index.php</a></p>
+                <p><a href="lab7/detail.php">detail.php</a></p>
+                <p><a href="lab7/form.php">form.php</a></p>
+            </section>
+
+            <section class="supporting-files">
+                <h3>Supporting files</h3>
+                <p><a href="lab7/css/custom.css">custom.css</a></p>
+                <p><a href="lab7/css/custom-tablet.css">custom-tablet.css</a></p>
+                <p><a href="lab7/css/custom-phone.css">custom-phone.css</a></p>
+                <p><a href="lab7/images/desktop-wireframe.jpeg">Desktop Wireframe</a></p>
+                <p><a href="lab7/images/tablet-wireframe.jpeg">Tablet Wireframe</a></p>
+                <p><a href="lab7/images/phone-wireframe.jpeg">Phone Wireframe</a></p>
+            </section>
+
+            <section class="grader-notes">
+                <h3>Notes to grader</h3>
+                <p></p>
+            </section> 
+        </section>
+
         <section class="grid-layout">
             <h2 class="header">Lab Six - Responsive Design.</h2>
             <section class="public-files">
@@ -101,9 +127,11 @@
             <section class="supporting-files">
                 <h3>Supporting files</h3>
                 <p><a href="lab6/css/custom.css">custom.css</a></p>
-                <p><a href="lab6/images/desktop-wireframe.jpeg">Desktop</a></p>
-                <p><a href="lab6/images/tablet-wireframe.jpeg">Tablet</a></p>
-                <p><a href="lab6/images/phone-wireframe.jpeg">Phone</a></p>
+                <p><a href="lab6/css/custom-tablet.css">custom-tablet.css</a></p>
+                <p><a href="lab6/css/custom-phone.css">custom-phone.css</a></p>
+                <p><a href="lab6/images/desktop-wireframe.jpeg">Desktop Wireframe</a></p>
+                <p><a href="lab6/images/tablet-wireframe.jpeg">Tablet Wireframe</a></p>
+                <p><a href="lab6/images/phone-wireframe.jpeg">Phone Wireframe</a></p>
             </section>
 
             <section class="grader-notes">
