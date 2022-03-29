bfc3a9e3ca00c737233bec0612ae724a291b4981
dakotasmith13 <97972068+dakotasmith13@users.noreply.github.com>
2022-03-28 22:10:58 -0400

    added detail, form, index files

--- lab7/index.php
+++ lab7/index.php
@@ -1,1 +1,107 @@
+<!DOCTYPE html>
+<html lang ="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Acts of Kindness for a Better World</title>
+        <meta name="author" content="Dakota Smith">
+        <meta name="description" content="In the chaos of today's world, 
+        spreading acts of kindness can make a positive impact, learn more here.">
+        <!-- Description is 107 characters, tries to promote a call to action -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" type="text/css">
+        <link rel="stylesheet" media="(max-width: 800px)" href="css/custom-tablet.css?version=<?php print time(); ?>" type="text/css">
+        <link rel="stylesheet" media="(max-width: 600px)" href="css/custom-phone.css?version=<?php print time(); ?>" type="text/css">
+    </head>
 
+    <body class="index">
+        <header>
+            <!-- logo image would go here -->
+            <h1>Spread Kindness</h1>
+            <h2>One Action to Change the World</h2>
+        </header>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="detail.php">Kind&nbsp;People</a>
+            <a href="form.php">Survey</a>
+        </nav>
+        <main>
+            <h1>Acts of Kindness</h1>
+            <section class="spread-kindness"> 
+                <h3>The World Needs More Kindness</h3>
+                <figure>
+                    <img alt="Kids sharing juicebox" src="images/kindness.jpg">
+                    <figcaption><cite><a href="https://en.wikipedia.org/wiki/Kindness" target="_blank">Wikipedia</a></cite></figcaption>
+                </figure>
+            </section>
+            <section class="kindness-paragraph">
+                <h2>Why Spread Kindness?</h2>
+                <p>In the chaos of today, the world needs a little more kindness. Being in a global
+                    pandemic for multiple years is not exactly a mental health booster. Watching the news
+                    is depressing- we see all of the negative events in the world highlighted into an hour long video. 
+                    The way that one rises above these events is through reaching out to those around them. A solid
+                    support system goes a long way today. Sometimes, all one needs is a little reassurance
+                    that they will be okay. Personally, I rely on this quite a bit. My friends do not have
+                    to tell me that they like my outfit, or that I am doing well in something, but they choose
+                    to because they are kind people.   
+                </p> 
+                <p>Kindness is underrated in today's world. Do you remember the last time a someone complimented you?
+                    How did it make you feel? I know that an unexpected compliment can turn a day around for me. 
+                    Wouldn't you like to be that person to make someone else's day a little better? 
+                    Remember- we cannot help others until we help ourselves. You must be kind to yourself
+                    so that you have the energy to help others. One cannot expect to be able to make a 
+                    difference when they are running on fumes. Treat yourself with love so that 
+                    you have enough love to give to others.
+                </p>
+            </section>
+
+            <section class="kindness-examples">
+                <h3>Examples of Kindness</h3>
+                <ul>
+                    <li>Giving someone a compliment</li>
+                    <li>Holding the door for someone</li>
+                    <li>Helping a friend with a task</li>
+                    <li>Paying for someone's order</li> 
+                    <li>Source: <cite>Dakota Smith</cite></li>
+                </ul>
+            </section>
+
+            <section class="compliment-table">
+                <h2>Compliment Ideas</h2>
+                <table>
+                    <caption>Some Topics to Consider</caption>
+                    <tr>
+                        <th></th>
+                        <th>Personality</th>
+                        <th>Appearance</th>
+                        <th>Lifestyle</th>
+                    </tr>
+                    <tr>
+                        <td rowspan="3">Compliments</td>
+                        <td>Kindness</td>
+                        <td>Outfit</td>
+                        <td>Doing great</td>
+                    </tr>
+                    <tr>
+                        <td>Intelligence</td>
+                        <td>Hair</td>
+                        <td>Work ethic</td>
+                    </tr>
+                    <tr>
+                        <td>Funny</td>
+                        <td>Smile</td>
+                        <td>Time management</td>
+                    </tr>
+                    <tr>
+                        <td colspan="4">Source: <cite><a href="https://www.verywellmind.com/positivity-boosting-compliments-1717559"
+                        target="_blank">https://www.verywellmind.com/positivity-boosting-compliments-1717559</a></cite></td>
+                    </tr>
+                </table>
+            </section>
+        </main>
+        <footer>
+            <p><a href="../index.php">Site map (Main index)</a></p>
+        </footer> 
+    </body>
+</html>
+
+        
